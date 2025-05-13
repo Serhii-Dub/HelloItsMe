9775455bd5b9577f8eb1f5e7b380c59d7af101ab
Serhii-Dub <im-24-dubrovin@kdpu.edu.ua>
2025-05-13 22:19:31 +0300

    Update planet.js

--- lost_island/sunshine/planet.js
+++ lost_island/sunshine/planet.js
@@ -1,23 +1,27 @@
 AFRAME.registerComponent('planet', {
   schema: {
-    dist: {type: 'number'},  // Відстань від Сонця
-    mass: {type: 'number'},  // Масса планети
-    name: {type: 'string'}   // Назва планети
+    dist: {type: 'number'},
+    mass: {type: 'number'},
+    name: {type: 'string'}
   },
   init: function () {
-    this.angle = Math.random() * 360;  // Початковий кут обертання
-    this.orbitRadius = this.data.dist / 1e9;  // Визначаємо орбітальний радіус
+    this.angle = Math.random() * 360;
   },
   tick: function (time, timeDelta) {
-    // Вираховуємо нове положення планети по колу
-    const speed = 0.1;  // Швидкість обертання планети
-    this.angle += (timeDelta / 1000) * speed;
-
+    const radius = this.data.dist / 1e9;
+    this.angle += (timeDelta / 1000) * 0.1;
     const rad = THREE.MathUtils.degToRad(this.angle);
-    const x = this.orbitRadius * Math.cos(rad);
-    const z = this.orbitRadius * Math.sin(rad);
-
-    // Оновлюємо позицію планети (враховуємо орбіту навколо Сонця)
+    const x = radius * Math.cos(rad);
+    const z = radius * Math.sin(rad);
     this.el.setAttribute('position', {x: x, y: 0, z: z});
+  }
+});
+
+AFRAME.registerComponent('spin', {
+  schema: {
+    speed: {type: 'number', default: 1}
+  },
+  tick: function (time, timeDelta) {
+    this.el.object3D.rotation.y += this.data.speed * timeDelta / 1000;
   }
 });
