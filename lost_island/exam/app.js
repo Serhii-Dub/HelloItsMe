d627ec17b9ab159898213d44700d13325ce6fccb
Serhii-Dub <im-24-dubrovin@kdpu.edu.ua>
2025-06-20 15:06:05 +0300

    some try

--- lost_island/exam/app.js
+++ lost_island/exam/app.js
@@ -2,8 +2,8 @@ const algorithms = [
   {
     name: "bubble_sort",
     code: `def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
+    "n = len(arr)"
+    " for i in range(n):"
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
